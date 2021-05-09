4f10c5b5a253feaf14e05db7973dc7b9b30eca92
unknown <81273310+ykay220@users.noreply.github.com>
2021-05-09 14:46:55 -0400

    Logic & styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import  { useState,useEffect } from 'react'
 import Card from './components/Card';
 import Currentcard from './components/Currentcard';
+import { CircularProgress } from '@material-ui/core';
 
 import axios from 'axios';
 import './App.css';
@@ -15,32 +16,44 @@ const [forecast, updateForecast] = useState()
 
 
 
+const getApi = (cityString) => {
 
+
+  axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityString}&units=metric&appid=79f6200497f5af385065493b315b6ea2`).then((res) => {
+    const currentData = res.data;
+    
+    updateQuery("")
+    updateWeather(currentData)
+    
+    
+    axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${currentData.coord.lat}&lon=${currentData.coord.lon}&exclude=current,hourly,minutely,alerts&units=metric&appid=79f6200497f5af385065493b315b6ea2`).then((res) => {
+      const forecastData = res.data;
+
+updateForecast(forecastData)
+
+
+
+
+ 
+})
+
+  })
+
+
+
+
+}
 
 
 
 const search = (evt) => {
   if (evt.key === "Enter"){
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=79f6200497f5af385065493b315b6ea2`).then((res) => {
-      const currentData = res.data;
-      
-      updateQuery("")
-      updateWeather(currentData)
-      
-      
-      axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${currentData.coord.lat}&lon=${currentData.coord.lon}&exclude=current,hourly,minutely,alerts&units=metric&appid=79f6200497f5af385065493b315b6ea2`).then((res) => {
-        const forecastData = res.data;
  
-  updateForecast(forecastData)
 
-  
-
-
-  
-})
-
-    })
+    getApi(query);
    
+  }else{
+    <CircularProgress/>
   }
 }
 
@@ -57,11 +70,19 @@ const queryHandler=(e)=>{
 
   return (
     <div className="main">
-        
      <div className="banner">
        <div className="banner-text"><h1>GET THE DAILY FORECAST FOR 5 DAYS</h1></div>
+       
         <div className="banner-inputdiv"><input className="search-input" onChange={queryHandler} value={query} onKeyPress={search} type="text" placeholder="Search for a city.."/></div>
      </div>
+
+      <div className="predefined-container">
+          <div className="button-predefinedcity" onClick={()=>getApi('Toronto')}>Toronto</div>
+          <div className="button-predefinedcity" onClick={()=>getApi('New York')}>New York</div>
+          <div className="button-predefinedcity" onClick={()=>getApi('London')}>London</div>
+      </div>
+    
+    
      <div className="section">
       
     {(typeof weather != "undefined") ? (  <div className="current-weatherdiv">
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -46,7 +46,7 @@ const forecastweekday=()=>{
                     </div>
                     
             </div>)
-        }
+        } 
     }
 
     return (
